be0538e05bbd16a3a46d35312961dffb6a093c60
Timofey Samsonov <tsamsonov@geogr.msu.ru>
2022-02-28 15:10:56 +0300

    add uuid and organizer

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
 import EventDetails from '../views/EventDetails.vue'
+import EventCreate from '../views/EventCreate.vue'
 import About from '../views/About.vue'
 
 const routes = [
@@ -14,6 +15,11 @@ const routes = [
     name: 'EventDetails',
     component: EventDetails,
     props: true
+  },
+  {
+    path: '/event/create',
+    name: 'EventCreate',
+    component: EventCreate
   },
   {
     path: '/About',
